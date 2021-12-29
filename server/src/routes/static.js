37754833a0eff7502fec245e7fb75f5f2146c6ea
mazrean <shunsuke.w04@gmail.com>
2021-12-29 14:14:26 +0900

    iroiro

--- server/src/routes/static.js
+++ server/src/routes/static.js
@@ -1,6 +1,7 @@
 import history from 'connect-history-api-fallback';
 import Router from 'express-promise-router';
 import serveStatic from 'serve-static';
+import compression from 'compression';
 
 import { CLIENT_DIST_PATH, PUBLIC_PATH, UPLOAD_PATH } from '../paths';
 
@@ -8,6 +9,7 @@ const router = Router();
 
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 router.use(history());
+router.use(compression());
 
 router.use(
   serveStatic(UPLOAD_PATH, {
