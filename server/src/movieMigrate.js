d8b175fbf457496f84c7260e03c07fc3d1a135b2
mazrean <shunsuke.w04@gmail.com>
2021-12-30 17:37:07 +0900

    webm修正

--- server/src/movieMigrate.js
+++ server/src/movieMigrate.js
@@ -27,7 +27,7 @@ import { ffmpeg } from './ffmpeg';
 
       const data = await fs.readFile(fp)
       ffmpeg.FS('writeFile', 'file', new Uint8Array(data))
-      await ffmpeg.run('-i', 'file', '-movflags', 'faststart', '-pix_fmt', 'yuv420p', '-row-mt', `crop=${cropOptions}`, '-an', name);
+      await ffmpeg.run('-i', 'file', '-movflags', 'faststart', '-pix_fmt', 'yuv420p', '-row-mt', '1', '-vf', `crop=${cropOptions}`, name);
       const exported = ffmpeg.FS('readFile', name)
       await fs.writeFile(filePath, exported, 'binary')
     }
