cc3b035b913ca7d5eb0bc1e3705e243aa4572ef7
mazrean <shunsuke.w04@gmail.com>
2021-12-30 18:00:19 +0900

    prefersReducedMotion

--- client/src/components/foundation/PausableMovie/PausableMovie.jsx
+++ client/src/components/foundation/PausableMovie/PausableMovie.jsx
@@ -14,10 +14,11 @@ import { FontAwesomeIcon } from '../FontAwesomeIcon';
  * @type {React.VFC<Props>}
  */
 const PausableMovie = ({ src }) => {
+  const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
   /** @type {React.RefObject<HTMLVideoElement>} */
   const videoRef = React.useRef(null);
 
-  const [isPlaying, setIsPlaying] = React.useState(true);
+  const [isPlaying, setIsPlaying] = React.useState(!prefersReducedMotion);
   const handleClick = React.useCallback(() => {
     setIsPlaying((isPlaying) => {
       if (isPlaying) {
@@ -32,7 +33,7 @@ const PausableMovie = ({ src }) => {
   return (
     <AspectRatioBox aspectHeight={1} aspectWidth={1}>
       <button className="group relative block w-full h-full" onClick={handleClick} type="button">
-        <video src={src} ref={videoRef} className="w-full" muted autoPlay={true} loop />
+        <video src={src} ref={videoRef} className="w-full" muted autoPlay={!prefersReducedMotion} loop />
         <div
           className={classNames(
             'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
